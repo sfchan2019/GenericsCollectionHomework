380ea2655c3bd0f109877dc445e615444c497c0b
DESKTOP-1ORJFAV\Sing <sing840722@gmail.com>
2019-09-03 21:18:13 +0100

    Working on Stack

--- GenericsCollectionHomework/Program.cs
+++ GenericsCollectionHomework/Program.cs
@@ -5,59 +5,146 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-/*
- [17:06] Markson Aigbodi
-    
-
-Labs
-
-Let's put some of this into practice
-
-Lab 1 : Cubic Array 10x10x10
-
-Build a three dimensional array and populate it with numbers which are products of the input numbers. Make the size of the cube to be 10x10x10 and your numbers will run from 1 at (1,1,1) through 1000 at (10,10,10). Numbering starts at 1 here instead of 0.
-
-Tip : use a loop within a loop within a loop to accomplish this!
-
-Finally output the value at (2,3,7) and check it has value 2x3x7=42
-
-Lab 2 : List
-
-Create a one-dimensional list of integers called List01.
-
-Iterate over the three dimensional array above, and for every number in the array, add it to the list.
-
-Finally add up the total sum of all numbers in the list and output the result.
-
-Lab 3 : Dictionary
-
-Create a dictionary of 10 countries as the index, with the capital city as the data.
-
-Iterate over the data and display it.
-
-Return the completed dictionary to complete the test
-
-Lab 4 : Queue
-
-Count from 1 to 100 and add the cubes of each number to a queue.
-
-Run dequeue 10 times and sum the output
-
-Return this sum.
-
-Lab 5 : Stack
-
-Repeat this for a stack ie count from 1 to 100, add the cube of each number to the stack, then pop 10 items off the top of the stack and sum the output of those 10 numbers.
-
-Return this sum
-     */
-
 namespace GenericsCollectionHomework
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Application app = new Application();
+            app.Run();
+        }
+    }
+
+    class Application
+    {
+        public void Run()
+        {
+            CollectionLab lab = new CollectionLab();
+            lab.InitializeCubicArray();
+            lab.PrintCubicArray(1, 1, 1);
+            lab.PrintCubicArray(10, 10, 10);
+            lab.PrintCubicArray(2, 3, 7);
+
+            lab.InitializeList();
+            Console.WriteLine(lab.SumOfList());
+
+            lab.InitializeDictionary();
+            lab.PrintCountryDictionary();
+
+            lab.InitializeQueue();
+            Console.WriteLine(lab.SumOfQueue());
+
+            lab.InitializeStack();
+
+        }
+    }
+
+    class CollectionLab
+    {
+        private int[,,] cubicArray;
+        private List<int> list01;
+        private Dictionary<string, string> countryDictionary;
+        private Queue<int> queue01;
+        private Stack<int> stack01;
+
+        public void InitializeCubicArray()
+        {
+            cubicArray = new int[11, 11, 11];
+            for (int i = 1; i < cubicArray.GetLength(0); i++)
+            {
+                for (int j = 1; j < cubicArray.GetLength(1); j++)
+                {
+                    for (int k = 1; k < cubicArray.GetLength(2); k++)
+                    {
+                        cubicArray[i, j, k] = i * j * k;
+                    }
+                }
+            }
+        }
+
+        public void PrintCubicArray(int i, int j, int k)
+        {
+            Console.WriteLine(cubicArray[i, j, k]);
+        }
+
+        public void InitializeList()
+        {
+            list01 = new List<int>();
+            foreach (int i in cubicArray)
+            {
+                list01.Add(i);
+            }
+        }
+
+        public int SumOfList()
+        {
+            int sum = 0;
+            foreach (int i in list01)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        public void InitializeDictionary()
+        {
+            //country and city list https://www.countries-ofthe-world.com/capitals-of-the-world.html
+            countryDictionary = new Dictionary<string, string>();
+            countryDictionary.Add("Australia", "Canberra");
+            countryDictionary.Add("Belgium", "Brussels");
+            countryDictionary.Add("China", "Beijing");
+            countryDictionary.Add("Denmark", "Copenhagen");
+            countryDictionary.Add("France", "Paris");
+            countryDictionary.Add("Greece", "Athens");
+            countryDictionary.Add("Italy", "Rome");
+            countryDictionary.Add("Japan", "Tokyo");
+            countryDictionary.Add("Spain", "Madrid");
+            countryDictionary.Add("United Kingdom", "London");
+        }
+
+        public void PrintCountryDictionary()
+        {
+            foreach (KeyValuePair<string, string> pair in countryDictionary)
+            {
+                Console.WriteLine(String.Format("The capital city of {0} is {1}", pair.Key, pair.Value));
+            }
+        }
+
+        public void InitializeQueue()
+        {
+            queue01 = new Queue<int>();
+            foreach (int i in cubicArray)
+            {
+                queue01.Enqueue(i);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                queue01.Dequeue();
+            }
+        }
+        public int SumOfQueue()
+        {
+            int sum = 0;
+            foreach (int i in queue01)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        public void InitializeStack()
+        {
+            stack01 = new Stack<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                stack01.Push(i);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                stack01.Pop();
+            }
         }
     }
 }
